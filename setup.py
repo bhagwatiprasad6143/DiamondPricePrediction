c6b6691a23947facf1e49896def1e1905e81a857
Bhagwati Prasad <nehrugarh12355ravi@gmail.com>
2024-04-05 17:24:40 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages ,setup
+from typing import List
+
+
+HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+        return requirements
+
+
+
+
+
+setup(
+
+    name='DiamondPricePrediction',
+    version='0.0.1',
+    author='Bhagwati Prasad',
+    author_email='nehrugarh12355ravi@gmail.com',
+    requires= get_requirements('requirement.txt'),
+    packages= find_packages()
+
+
+    
+
+
+
+)
