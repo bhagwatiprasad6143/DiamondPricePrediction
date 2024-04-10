e633702215cb8ae0362d81a404d4a8905fa5bddc
Bhagwati Prasad <nehrugarh12355ravi@gmail.com>
2024-04-10 11:36:39 +0530

    first commit

--- src/utils.py
+++ src/utils.py
@@ -1,31 +1,25 @@
 import os
 import sys
-import pandas as pd
-import numpy as np
-
-from src.logger import logging
-from src.exception import CustomException
-
 import pickle
-from sklearn.metrics import r2_score , mean_absolute_error , mean_squared_error
+import numpy as np 
+import pandas as pd
+from src.exception import CustomException
+from src.logger import logging
 
+from sklearn.metrics import r2_score,mean_absolute_error,mean_squared_error
 
-def save_object(file_path , obj):
-
+def save_object(file_path, obj):
     try:
-
         dir_path = os.path.dirname(file_path)
-        os.makedirs(dir_path , exist_ok=True)
 
-        with open(file_path , 'wb') as file_obj:
-            pickle.dump(obj , file_obj)
+        os.makedirs(dir_path, exist_ok=True)
 
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
 
     except Exception as e:
-        raise CustomException(e ,sys)
+        raise CustomException(e, sys)
     
-
-
 def evaluate_model(X_train,y_train,X_test,y_test,models):
     try:
         report = {}
@@ -52,17 +46,10 @@ def evaluate_model(X_train,y_train,X_test,y_test,models):
             raise CustomException(e,sys)
     
 
-def load_object(self , file_path):
-     
+def load_object(file_path):
     try:
-
-        with open(file_path , 'rb') as file_obj:
-            retrun pickle.load(file_obj)
-
-
+        with open(file_path,'rb') as file_obj:
+            return pickle.load(file_obj)
     except Exception as e:
-        logging.info('error occured in pickle loadting')
-
-        raise CustomException(e:sys)
-          
-    
+        logging.info('Exception Occured in load_object function utils')
+        raise CustomException(e,sys)
